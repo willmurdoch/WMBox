a64309e2dc70703c51168d9ebfa3567d4228ae44
willm <willmurdoch@gmail.com>
2017-03-26 16:16:25 -0400

    Minimized HTML reconstruction

--- js/wmBox.js
+++ js/wmBox.js
@@ -1,12 +1,12 @@
 (function($){
 	$.wmBox = function(){
-		$('body').prepend('<div class="wmBox_overlay"><div class="wmBox_centerWrap"><div class="wmBox_centerer">');
+		$('body').prepend('<div class="wmBox_overlay"><div class="wmBox_centerWrap"><div class="wmBox_centerer"><div class="wmBox_contentWrap"><div class="wmBox_scaleWrap"><div class="wmBox_closeBtn"><p>x</p></div>');
 	};
 	$('[data-popup]').click(function(e){
 		e.preventDefault();
 		$('.wmBox_overlay').fadeIn(750);
 		var mySrc = $(this).attr('data-popup');
-		$('.wmBox_overlay .wmBox_centerer').html('<div class="wmBox_contentWrap"><div class="wmBox_scaleWrap"><div class="wmBox_closeBtn"><p>x</p></div><iframe src="'+mySrc+'">');
+		$('.wmBox_overlay .wmBox_scaleWrap').append('<iframe src="'+mySrc+'">');
 		
 		$('.wmBox_overlay iframe').click(function(e){
 			e.stopPropagation();
